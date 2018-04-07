cf4d1110c363ad81eb55375fb72aaf17d033b476
jendiamond <thejendiamond@gmail.com>
2018-04-07 16:22:05 -0700

    Add Assignment 3

---
+++ 02_domInteraction/Assignment3/jendiamond/main.js
@@ -0,0 +1,24 @@
+new Vue({
+  el: '#app',
+  data: {
+    value: 0,
+  },
+  computed: {
+    output: function(value) {
+      return this.value < 37 ? 'not there yet' : 'done';
+    }
+  },
+  watch: {
+    value: function(value) {
+      var vm = this;
+      setTimeout(function(){
+        vm.value = 0;
+      });
+    }
+  },
+  methods: {
+    result: function(value) {
+      return this.value < 37 ? 'not there yet' : 'done';
+    }
+  }
+});
